5c0c659c32983d1f358da3e98afa0c43f21a5ad8
kknoriakidev <94515421+kknoriakidev@users.noreply.github.com>
2022-02-08 10:21:46 +0400

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,4 @@
+const MONITOR_SERVER_ADDERS   = '95.62.52.66',                // Адрес сервера
+      MONITOR_SERVER_PORT     = '28532',                    // Порт сервера. Диапозон от 20000 до 30000
+      MONITOR_SERVER_ENGINE   = 0,                          // Тип движка. 0 - Source, 1 - GoldSource.
+      MONITOR_BASE_URL        = 'https://vk.com/phadpublics'; // Ссылка на обработчик. Подробнее: https://goo.gl/856nuZ
